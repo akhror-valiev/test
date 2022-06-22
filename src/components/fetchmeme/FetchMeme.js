1aa395378ae0c6cc787909f0840b01592a539be3
akhror-valiev <ahror.valiev@gmail.com>
2022-06-22 19:30:34 +0500

    second

--- src/components/fetchmeme/FetchMeme.js
+++ src/components/fetchmeme/FetchMeme.js
@@ -1,97 +1,61 @@
 import { useState, useEffect } from "react";
-import './meme.css'
-import { Link } from "react-router-dom"
-
-
-
-
-
-
-
+import "./meme.css";
 
 
 const FetchMeme = () => {
-
-    const [memes, setMemes] = useState([])
-    const [SearchMeme, setSearchMeme] = useState("")
-
-
-
-
-
-
-
-
+    const [memes, setMemes] = useState([]);
+    const [SearchMeme, setSearchMeme] = useState("");
 
     useEffect(() => {
-        fetch('https://api.imgflip.com/get_memes')
+        fetch("https://api.imgflip.com/get_memes")
             .then((res) => {
-                return res.json()
+                return res.json();
             })
 
             .then((data) => {
-                console.log(data.data.memes)
-                setMemes(data.data.memes)
-
-
-            })
-    }, [])
-
-
+                console.log(data.data.memes);
+                setMemes(data.data.memes);
+            });
+    }, []);
 
     return (
         <>
             <div className="headerSearchItem">
-
                 <input
                     type="text"
-
                     onChange={(e) => setSearchMeme(e.target.value)}
                     placeholder="Search..."
                     className="headerSearchInput"
-
                 />
-
-
             </div>
             <div className="container">
+                {memes
 
+                    .filter((value) => {
+                        if (SearchMeme === "") {
+                            return value;
+                        } else if (
+                            value.name.toLowerCase().includes(SearchMeme.toLowerCase())
+                        ) {
+                            return value;
+                        }
+                    })
 
+                    .map((mem) => (
+                        <div className="slider" keys={mem.id}>
+                            <h4 className="title">{mem.name}</h4>
 
-
-                {
-                    memes
-
-
-                        .filter((value) => {
-                            if (SearchMeme === "") {
-                                return value
-                            } else if (
-                                value.name.toLowerCase().includes(SearchMeme.toLowerCase())) {
-                                return value
-                            }
-
-                        })
-
-
-                        .map((mem) => (
-                            <div className="slider" keys={mem.id}>
-
-                                <h4 className="title">{mem.name}</h4>
-
-                                <img className="sliderImg" src={mem.url} alt={mem.name} classname="img" />
-
-                            </div>
-                        ))
-                }
-
+                            <img
+                                className="sliderImg"
+                                src={mem.url}
+                                alt={mem.name}
+                                classname="img"
+                            />
+                        </div>
+                    ))}
             </div>
         </>
     );
-}
-
-
-
-
+};
 
 export default FetchMeme;
